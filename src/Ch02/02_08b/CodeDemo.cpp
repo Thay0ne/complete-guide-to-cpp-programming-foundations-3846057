4e0574cbecdfb26c9d3a83115ab9e72edc13ebdc
Gina González <thaayone@gmail.com>
2025-08-11 02:28:47 +0000

    Added code to print var sound value

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -13,7 +13,7 @@ int main(){
     asset_type asset_value;
     asset_value = asset_type::sound;
 
-    std::cout << "asset_value = " << (int)asset_value << std::endl;
+    std::cout << "asset_value = " << (int)asset_value << std::endl << "sound = " << sound << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
